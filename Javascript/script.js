5060977bbc6b9d9d3f1b09c9111fa1a52c6dcf60
Roberto Isaac <20220397@itla.edu.do>
2023-05-01 19:10:25 -0500

    Actualizacion V2

--- Javascript/script.js
+++ Javascript/script.js
@@ -3,15 +3,32 @@ const form = document.querySelector("#obtn");
 const nameCity = document.querySelector("#city");
 const nameCountry = document.querySelector("#country");
 
-form.addEventListener("submit", (e) =>{
-    e.preventDefault();
-    if(nameCity.value === "" || nameCountry.value === "" ){
-        mostrarError("Los campos son obligatorios!");
-    }
-    // console.log(nameCity.value);
-    // console.log(nameCountry.value);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (nameCity.value === "" || nameCountry.value === "") {
+    MostrarError("Los campos son obligatorios!");
+    return;
+  }
+
+  ApiCall(nameCity.value, nameCountry.value);
+  // console.log(nameCity.value);
+  // console.log(nameCountry.value);
 });
 
-function mostrarError(mensaje){
-    console.log(mensaje);
+function ApiCall (city,country){  
+  const apiId = ` 6b0558ab6d882ecc1dfd9fc4c9c6ea00`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiId}`
+}
+
+function MostrarError(mensaje){
+  console.log(mensaje)
+  const alert = document.createElement("p");
+  alert.classList.add("Message-alert");
+  alert.innerHTML = mensaje;
+
+  form.appendChild(alert);
+
+  setTimeout(() =>{
+    alert.remove()
+  },3500);
 }
